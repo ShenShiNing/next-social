e4c71723a7d2d352eb8e62da28afb1deb7e96db7
Shenshining <353824385@qq.com>
2024-11-23 03:26:34 +0800

    初始化导航栏以及主页CenterMenu的Stories和Post部分

---
+++ src/app/components/AddPost.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const AddPost = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/27940276/pexels-photo-27940276.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="avatar"
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* POST */}
+      <div className="flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4">
+          <textarea placeholder="What's on your mind?" className="flex-1 bg-slate-100 rounded-lg p-2"></textarea>
+          <Image
+            src="/emoji.png"
+            alt="emoji"
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" alt="addImage" width={20} height={20}/>
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="addVideo" width={20} height={20}/>
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="poll" width={20} height={20}/>
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="addEvent" width={20} height={20}/>
+            Event
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddPost
---
+++ src/app/components/Stories.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image"
+
+const Stories = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* STORIES */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image src="https://images.pexels.com/photos/28958306/pexels-photo-28958306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+          <span className="font-medium">Ricky</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Stories
